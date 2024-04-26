0ea6c48fa9613f15c38417be81b3f9795742f3b1
Andrea-Reyes <karen.reyeslem@gmail.com>
2024-04-25 23:29:56 -0600

    Creación index.php
    Creación del archivo index y la carpeta para las imágenes

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php require_once "config/conexion.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qawa'</title>
+    <link rel="icon" type="image/x-icon" href="imagenes/icono.webp" />
+</head>
+
+
+<body>
+    <header class="bg-dark py-5">
+        <div class="container px-4 px-lg-5 my-5">
+            <div class="text-center text-white">
+                <h1 class="display-4 fw-bolder">Qawa'</h1>
+                <p class="lead fw-normal text-white-50 mb-0">Cafeteria UVG</p>
+            </div>
+        </div>
+    </header>
+    
+</body>
+</html>
