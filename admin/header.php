27ed93dd686ca7024f96aa4a45388db64b7f6926
SoIsaac7 <150219669+SoIsaac7@users.noreply.github.com>
2024-06-10 21:15:10 -0600

    Cambios realizados
    Realicé algunas modificaciones en dichos archivos.

--- admin/header.php
+++ admin/header.php
@@ -16,6 +16,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="css/imagenes/logo2.webp" />
         <link href="almuerzosAdmin.css" rel="stylesheet" /> 
+        <link rel="stylesheet" href="estilo_flotante.css">
         <title>Almuerzos</title>
     </head>
     <body>
--- configuraciones/config.php
+++ configuraciones/config.php
@@ -1,4 +1,15 @@
 <?php
+define("KEY_TOKEN", "Def.wqwa-298*");
 define('CLIENT_ID', '');
 define('LOCALE', 'es_ES');
+define('MONEDA', 'Q ');
+//Para que inicie la sesión una vez el usuario ingresa a la página
+session_start();
+
+//Para tener el número de tipos de almuerzos
+$numero_carro = 0;
+if(isset($_SESSION['carrito']['productos'])){
+    $numero_carro = count($_SESSION['carrito']['productos']);
+}
+
 ?>
